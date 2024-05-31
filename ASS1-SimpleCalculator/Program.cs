e7f623a8fbff2d24211160914ec47348d4125ff2
Saida Shaik <saidashaik6293@gmail.com>
2024-05-31 23:46:43 +0530

    Calc Coding Part I - Done.

--- ASS1-SimpleCalculator/Program.cs
+++ ASS1-SimpleCalculator/Program.cs
@@ -1,2 +1,44 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+int firstnum = 0;
+int secondnum = 0;
 Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Input the First number:");
+string firstnumIP = Console.ReadLine();
+firstnum = int.Parse(firstnumIP);
+
+Console.WriteLine("Input the Second number:");
+string secondnumIP = Console.ReadLine();
+secondnum = int.Parse(secondnumIP);
+
+var final = 0;
+Console.WriteLine("What do you want to do?");
+Console.WriteLine("[A]dd numbers.");
+Console.WriteLine("[S]ubstract numbers");
+Console.WriteLine("[M]ultiplay numbers");
+
+var resultReq = Console.ReadLine();
+if (resultReq != null)
+{
+    if (resultReq.Equals("A") || resultReq.Equals("a"))
+    {
+        final = firstnum + secondnum;
+    }
+    else if (resultReq.Equals("S") || resultReq.Equals("s"))
+    {
+        final = firstnum - secondnum;
+    }
+    else if (resultReq.Equals("M") || resultReq.Equals("m"))
+    {
+        final = firstnum * secondnum;
+    }
+    else
+    {
+        Console.WriteLine("Please enter valid input");
+    }
+}
+else
+{
+    Console.WriteLine("Please enter valid input");
+}
