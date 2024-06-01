6cb76bbf082b0f60603100adf99e4ba753f4cef6
Saida Shaik <saidashaik6293@gmail.com>
2024-06-02 01:15:43 +0530

    While Loop

---
+++ WhileLoop/Program.cs
@@ -0,0 +1,54 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        /*
+        int num = 0;
+
+        while (num < 10)
+        {
+            //num = num + 1;
+
+            //OR
+            //num += 2;
+
+            //OR
+            //num++;
+
+            //OR
+            ++num;
+            Console.WriteLine(num);
+        }
+        Console.WriteLine("Lood completed!!");
+        */
+
+        /*
+        var userInput = Console.ReadLine();
+        while (userInput.Length < 10)
+        {
+            userInput += '*';
+            Console.WriteLine(userInput);
+        }
+        */
+        Console.WriteLine( "Enter First Number:");
+        var firstInput = Console.ReadLine();
+        int firstNumber = int.Parse(firstInput);
+
+        Console.WriteLine("Enter Last Number:");
+        var secondInput = Console.ReadLine();
+        int lastNumber = int.Parse(secondInput);
+        
+        int res = 0;
+        while (firstNumber < lastNumber)
+        {
+            firstNumber++;
+            Console.WriteLine(firstNumber);
+             res = (firstNumber + firstNumber);
+            
+        }
+        Console.WriteLine("Result :" + res);
+
+    }
+}
