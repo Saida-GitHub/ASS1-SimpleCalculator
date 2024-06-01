9a8ab2051d280871346e340ee36e1348f5aad31d
Saida Shaik <saidashaik6293@gmail.com>
2024-06-02 00:30:19 +0530

    ASS1-Switch.

--- ASS1-SimpleCalculator/Program.cs
+++ ASS1-SimpleCalculator/Program.cs
@@ -1,45 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-int firstnum = 0;
-int secondnum = 0;
-Console.WriteLine("Hello, World!");
-
-Console.WriteLine("Input the First number:");
-string firstnumIP = Console.ReadLine();
-firstnum = int.Parse(firstnumIP);
-
-Console.WriteLine("Input the Second number:");
-string secondnumIP = Console.ReadLine();
-secondnum = int.Parse(secondnumIP);
-
-var final = 0;
-Console.WriteLine("What do you want to do?");
-Console.WriteLine("[A]dd numbers.");
-Console.WriteLine("[S]ubstract numbers");
-Console.WriteLine("[M]ultiplay numbers");
-
-var resultReq = Console.ReadLine();
-if (resultReq != null)
+﻿internal class Program
 {
-    if (resultReq.Equals("A") || resultReq.Equals("a"))
+    private static void Main(string[] args)
     {
-        final = firstnum + secondnum;
+        int firstnum = 0;
+        int secondnum = 0;
+        Console.WriteLine("Hello, World!");
+
+        Console.WriteLine("Input the First number:");
+        string firstnumIP = Console.ReadLine();
+        firstnum = int.Parse(firstnumIP);
+
+        Console.WriteLine("Input the Second number:");
+        string secondnumIP = Console.ReadLine();
+        secondnum = int.Parse(secondnumIP);
+
+        var final = 0;
+        Console.WriteLine("What do you want to do?");
+        Console.WriteLine("[A]dd numbers.");
+        Console.WriteLine("[S]ubstract numbers");
+        Console.WriteLine("[M]ultiplay numbers");
+
+        var resultReq = Console.ReadLine();
+        if (resultReq != null)
+        {
+            if (resultReq.Equals("A") || resultReq.Equals("a"))
+            {
+                final = firstnum + secondnum;
+            }
+            else if (resultReq.Equals("S") || resultReq.Equals("s"))
+            {
+                final = firstnum - secondnum;
+            }
+            else if (resultReq.Equals("M") || resultReq.Equals("m"))
+            {
+                final = firstnum * secondnum;
+            }
+            else
+            {
+                Console.WriteLine("Please enter valid input");
+            }
+            Console.WriteLine("Result::" + final);
+        }
+        else
+        {
+            Console.WriteLine("Please enter valid input");
+        }
     }
-    else if (resultReq.Equals("S") || resultReq.Equals("s"))
-    {
-        final = firstnum - secondnum;
-    }
-    else if (resultReq.Equals("M") || resultReq.Equals("m"))
-    {
-        final = firstnum * secondnum;
-    }
-    else
-    {
-        Console.WriteLine("Please enter valid input");
-    }
-    Console.WriteLine("Result::" + final);
-}
-else
-{
-    Console.WriteLine("Please enter valid input");
 }
---
+++ Switch/Program.cs
@@ -0,0 +1,35 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        var userInput = Console.ReadLine();
+
+        if (!string.IsNullOrEmpty(userInput))
+        {
+            int Input = int.Parse(userInput);
+
+            switch (Input)
+            {
+                case 1:
+                case 2:
+                case 3:
+                case 4:
+                case 5:
+                    Console.WriteLine("Week Day");
+                    break;
+                case 6:
+                case 7:
+                    Console.WriteLine("WeekEnd");
+                    break;
+                default:
+                    Console.WriteLine("Invalid");
+                    break;
+            }
+        }
+        else
+        {
+            Console.WriteLine("Please enter input");
+        }
+    }
+}
